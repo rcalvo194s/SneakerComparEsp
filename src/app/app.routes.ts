05c1303ccfbfd53284c8c3e307f64e92acb43090
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-11 17:12:05 +0100

    he borrado una ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '' },
 ];

4faf018d0e91494d9e5f62099a54c74161a1e605
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-18 17:31:18 +0100

    inicio estructura de la pantalla de results sin scss

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    FormsModule,
+    CommonModule
+],
   templateUrl: './results.component.html',
-  styleUrl: './results.component.scss'
+  styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent {
+  searchQuery: string = '';
+  sneakers = [
+    {
+      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      name: 'Air Max 90',
+      price: 100,
+      brand: 'Nike',
+    },
+    {
+      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      name: 'Yeezy Boost 350',
+      price: 200,
+      brand: 'Adidas',
+    },
+  ];
 
+  onSearch() {
+    console.log(`Buscando sneakers con la query: ${this.searchQuery}`);
+  }
 }

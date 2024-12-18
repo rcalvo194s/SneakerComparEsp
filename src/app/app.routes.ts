0e65829713db6c08cd11dc3f4afc5e0956d6937d
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-18 16:59:20 +0100

    muevo a una nueva carpeta las páginas de la web, actualizo el archivo de rutas y la ruta de la imagen de fondo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ResultsComponent } from './pages/results/results.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'results', component: ResultsComponent },
   { path: '**', redirectTo: '' },
 ];

6a9fea39e9f43e2d39c5f98bf1849ce5471d771b
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-21 18:46:56 +0100

    terminado diseño de results con componentes por el momento

---
+++ src/app/models/sneaker.model.ts
@@ -0,0 +1,9 @@
+export interface Sneaker {
+    id: string;
+    imagenUrl: string;
+    marca: string;
+    modelo: string;
+    nombre: string;
+    precio: number;
+}
+  
--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { Sneaker } from '../../models/sneaker.model';
 
 @Component({
   selector: 'app-results',
@@ -12,60 +13,61 @@ import { SearchBarComponent } from '../../components/search-bar/search-bar.compo
     FormsModule,
     CommonModule,
     NavToolbarComponent,
-    SearchBarComponent
+    SearchBarComponent,
 ],
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent {
-  sneakers = [
+  
+  sneakers: Sneaker[] = [
     {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Air Max 90',
-      price: 100,
-      brand: 'Nike',
+      id: '0',
+      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      nombre: 'Air Max 90',
+      precio: 100,
+      marca: 'Nike',
+      modelo: 'Blue Royal'
     },
     {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
+      id: '1',
+      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      nombre: 'Yeezy',
+      precio: 200,
+      marca: 'Adidas',
+      modelo: 'Boost 350'
     },
     {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
+      id: '0',
+      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      nombre: 'Air Max 90',
+      precio: 100,
+      marca: 'Nike',
+      modelo: 'Blue Royal'
     },
     {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
+      id: '1',
+      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      nombre: 'Yeezy',
+      precio: 200,
+      marca: 'Adidas',
+      modelo: 'Boost 350'
     },
     {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
+      id: '0',
+      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      nombre: 'Air Max 90',
+      precio: 100,
+      marca: 'Nike',
+      modelo: 'Blue Royal'
     },
     {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
-    },
-    {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
-    },
-    {
-      image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      name: 'Yeezy Boost 350',
-      price: 200,
-      brand: 'Adidas',
+      id: '1',
+      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
+      nombre: 'Yeezy',
+      precio: 200,
+      marca: 'Adidas',
+      modelo: 'Boost 350'
     },
   ];
 }

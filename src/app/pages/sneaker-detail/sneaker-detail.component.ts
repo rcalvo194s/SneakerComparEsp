fbbf2b80c5579875e8267090e01f6038be22e273
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-23 13:40:38 +0100

    diseño bastante avanzado de la página de detalle de la zapatilla

--- src/app/pages/sneaker-detail/sneaker-detail.component.ts
+++ src/app/pages/sneaker-detail/sneaker-detail.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { Sneaker } from '../../models/sneaker.model';
 
 @Component({
   selector: 'app-sneaker-detail',
   standalone: true,
   imports: [
-    NavToolbarComponent
+    CommonModule,
+    FormsModule,
+    NavToolbarComponent,
+    SearchBarComponent
   ],
   templateUrl: './sneaker-detail.component.html',
-  styleUrl: './sneaker-detail.component.scss'
+  styleUrls: ['./sneaker-detail.component.scss']
 })
-export class SneakerDetailComponent {
+export class SneakerDetailComponent implements OnInit {
+  sneaker: Sneaker = {
+    id: '1',
+    imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg',
+    nombre: 'Air Jordan 1 Retro High OG',
+    precio: 170,
+    marca: 'Nike',
+    modelo: 'Chicago Reimagined',
+  };
 
+  compareTable = [
+    { web: 'StockX', precio: 220, enlace: 'https://www.stockx.com' },
+    { web: 'GOAT', precio: 215, enlace: 'https://www.goat.com' },
+    { web: 'Flight Club', precio: 230, enlace: 'https://www.flightclub.com' }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
 }

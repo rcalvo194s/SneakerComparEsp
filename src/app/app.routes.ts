853d1354321b68da36d5a52f2e99d1bd02259aec
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-23 19:03:03 +0100

    primer diseño de la página de Acerca de y ruta de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ResultsComponent } from './pages/results/results.component';
 import { SneakerDetailComponent } from './pages/sneaker-detail/sneaker-detail.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'results', component: ResultsComponent },
   { path: 'detail', component: SneakerDetailComponent },
+  { path: 'aboutme', component: AboutMeComponent },
   { path: '**', redirectTo: '' },
 ];
--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [
+    NavToolbarComponent
+  ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })

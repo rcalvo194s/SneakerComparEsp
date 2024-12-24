3e918991594abea1beaeecf4b53419dcacda73a6
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-24 12:06:44 +0100

    elimino imports que he utilizado durante las pruebas

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
@@ -11,10 +8,7 @@ import { MatTabsModule } from '@angular/material/tabs';
   standalone: true,
   imports: [
     NavToolbarComponent,
-    CommonModule, 
-    MatCardModule, 
-    MatIconModule, 
-    MatButtonModule,
+    CommonModule,
     MatTabsModule
   ],
   templateUrl: './about-me.component.html',

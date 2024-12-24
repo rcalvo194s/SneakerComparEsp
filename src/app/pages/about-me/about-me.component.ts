f2336a3643f94640d0fca1db5514f3e7795a6f35
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-24 12:03:33 +0100

    página con tabs para mostrar las tarjetas de información acerca de la web y de mí

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [
-    NavToolbarComponent
+    NavToolbarComponent,
+    CommonModule, 
+    MatCardModule, 
+    MatIconModule, 
+    MatButtonModule,
+    MatTabsModule
   ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'

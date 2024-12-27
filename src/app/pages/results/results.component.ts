00efe17b364dde0418fd3ccb55c72f85737b514b
Ricardo Calvo <rcalvo194s@fpdrioja.com>
2024-12-27 16:21:48 +0100

    muestro datos reales de Firebase y cambios en el diseño de la página de Búsqueda

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { Sneaker } from '../../models/sneaker.model';
-import { RouterModule } from '@angular/router';
+import { SneakersFirebaseService } from '../../../services/sneakers-firebase.service';
 
 @Component({
   selector: 'app-results',
@@ -20,62 +21,19 @@ import { RouterModule } from '@angular/router';
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit {
   
-  sneakers: Sneaker[] = [
-    {
-      id: '0',
-      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      marca: 'Nike',
-      modelo: 'Blue Royal',
-      nombre: 'Air Max 90',
-      precioBase: 100,
-      precios: []
-    },
-    {
-      id: '1',
-      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      nombre: 'Yeezy',
-      marca: 'Adidas',
-      modelo: 'Boost 350',
-      precioBase: 200,
-      precios: []
-    },
-    {
-      id: '0',
-      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      nombre: 'Air Max 90',
-      marca: 'Nike',
-      modelo: 'Blue Royal',
-      precioBase: 100,
-      precios: []
-    },
-    {
-      id: '1',
-      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      nombre: 'Yeezy',
-      marca: 'Adidas',
-      modelo: 'Boost 350',
-      precioBase: 200,
-      precios: []
-    },
-    {
-      id: '0',
-      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      nombre: 'Air Max 90',
-      marca: 'Nike',
-      modelo: 'Blue Royal',
-      precioBase: 100,
-      precios: []
-    },
-    {
-      id: '1',
-      imagenUrl: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Chicago-Reimagined-GS-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=60&dpr=1&trim=color&updated_at=1669232518',
-      nombre: 'Yeezy',
-      marca: 'Adidas',
-      modelo: 'Boost 350',
-      precioBase: 200,
-      precios: []
-    },
-  ];
+  sneakers: Sneaker[] = [];
+
+  constructor(private sneakersService: SneakersFirebaseService) {}
+
+  ngOnInit() {
+    this.getSneakersFromFirebase();
+  }
+
+  getSneakersFromFirebase() {
+    this.sneakersService.getSneakers().subscribe(data => {
+      this.sneakers = data;
+    });
+  }
 }
